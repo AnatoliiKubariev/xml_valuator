3b130b7f1df80c937091fecaeffb40f1377dba54
Saragossa777 <saragossa75@gmail.com>
2015-09-27 16:40:36 +0300

    xml geteration routine added; wip

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <algorithm>
+#include <fstream>
 
 #include "thirdparty\pugixml\src\pugixml.hpp"
 
@@ -78,42 +80,128 @@ double calculate(const pugi::xml_node& node)
 		return division(node);
 }
 
+void add_addition(std::ostream& os, int id);
+void add_subtraction(std::ostream& os, int id);
+void add_multiplication(std::ostream& os, int id);
+void add_division(std::ostream& os, int id);
+
+void generate_xml(std::ostream& os)
+{
+	os << "<expressions>" << std::endl;
+	//for (int id = 1; id <= 4; ++id)
+	int id = 1;
+	while (os.tellp() < 1024 * 1024 * 1024)
+	{
+		int xml_case = rand() % 4 + 1;
+		switch (xml_case)
+		{
+		case 1:
+			add_addition(os, id);
+			break;
+		case 2:
+			add_subtraction(os, id);
+			break;
+		case 3:
+			add_multiplication(os, id);
+			break;
+		case 4:
+			add_division(os, id);
+			break;
+		default:
+			break;
+		}
+		++id;
+	}
+
+
+	os << "</expressions>";
+}
+
+void add_addition(std::ostream& os, int id)
+{
+	os << "<addition id=\"" << id << "\">" << std::endl;
+
+	for (int i = 0; i < rand() % 5 + 2; ++i)
+	{
+		os << "<item>";
+		int item = rand() % 100 + 1;
+		os << item;
+		os << "</item>" << std::endl;
+	}
+
+	os << "</addition>" << std::endl;
+}
+
+void add_subtraction(std::ostream& os, int id)
+{
+	os << "<subtraction id=\"" << id << "\">" << std::endl;
+
+	os << "<minuend>";
+	int minuend = rand() % 100 + 1;
+	os << minuend;
+	os << "</minuend>" << std::endl;
+
+	os << "<subtrahend>";
+	int subtrahend = rand() % 100 + 1;
+	os << subtrahend;
+	os << "</subtrahend>" << std::endl;
+
+	os << "</subtraction>" << std::endl;
+}
+
+void add_multiplication(std::ostream& os, int id)
+{
+	os << "<multiplication  id=\"" << id << "\">" << std::endl;
+
+	for (int i = 0; i < rand() % 5 + 2; ++i)
+	{
+		os << "<factor>";
+		int factor = rand() % 100 + 1;
+		os << factor;
+		os << "</factor>" << std::endl;
+	}
+
+	os << "</multiplication>" << std::endl;
+}
+
+void add_division(std::ostream& os, int id)
+{
+	os << "<division  id=\"" << id << "\">" << std::endl;
+
+	os << "<dividend>";
+	int dividend = rand() % 100 + 1;
+	os << dividend;
+	os << "</dividend>" << std::endl;
+
+	os << "<divisor>";
+	int divisor = rand() % 100 + 1;
+	os << divisor;
+	os << "</divisor>" << std::endl;
+
+	os << "</division>" << std::endl;
+}
+
 
 int main()
 {
+	//pugi::xml_document doc;
+	//if (!doc.load_file("data0002.xml"))
+	//	return -1;
+
+	{
+		std::ofstream file("file.xml");
+		generate_xml(file);
+	}
+
 	pugi::xml_document doc;
-	if (!doc.load_file("data0002.xml"))
+	if (!doc.load_file("file.xml"))
 		return -1;
 
 	std::cout << std::boolalpha;
 	for (pugi::xml_node node = doc.child("expressions").first_child(); node; node = node.next_sibling())
 	{
-		//Test
-		switch (node.attribute("id").as_int())
-		{
-		case 10:
-			std::cout << node.name() << "\n";
-			std::cout << (calculate(node) == 9) << "\n";
-			break;
-		case 11:
-			std::cout << node.name() << "\n";
-			std::cout << (calculate(node) == 1) << "\n";
-			break;
-		case 12:
-			std::cout << node.name() << "\n";
-			std::cout << (calculate(node) == 240) << "\n";
-			break;
-		case 13:
-			std::cout << node.name() << "\n";
-			std::cout << (calculate(node) == 1814400) << "\n";
-			break;
-		case 14:
-			std::cout << node.name() << "\n";
-			std::cout << (calculate(node) == 6) << "\n";
-			break;
-		default:
-			return -1;
-		}
+		std::cout << node.name() << std::endl;
+		std::cout << calculate(node) << std::endl;
 	}
 
 	std::cin.get();
